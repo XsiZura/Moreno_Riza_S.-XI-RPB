6e901aa460158d84c6698fccc17435ca8bec67a7
XsiZura <rizasaktiawanmoreno@gmail.com>
2024-12-19 10:12:56 +0700

    modifikasi penambahan dan menghapus

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require 'session.php';
-require 'koneksi.php';
 
 $produk = mysqli_query($koneksi, "SELECT id, nama_produk, harga, foto, detail FROM produk LIMIT 6");
 ?>
@@ -97,7 +96,7 @@ $produk = mysqli_query($koneksi, "SELECT id, nama_produk, harga, foto, detail FR
             </p>
         </div>
     </div>
-
+                    
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="fontawesome/js/all.min.js"></script>
 </body>
--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$db     = 'toko_online';
-
-$koneksi  = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
-    ?>
